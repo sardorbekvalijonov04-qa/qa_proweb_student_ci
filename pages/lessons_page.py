bd30cf1dca310d9e8d8b8d6c937a267311fae11c
Sardorbek Valijonov <sardorbekvalijonov04@gmail.com>
2026-03-04 14:47:01 +0500

    Update lessons_page.py

--- pages/lessons_page.py
+++ pages/lessons_page.py
@@ -10,8 +10,6 @@ class LessonsPage:
         self.last_lesson = (By.CSS_SELECTOR, "#app > div > div.container.container_mobile > div > div > div.new-lessons_content > div > div:nth-child(5) > div.flex.gap20 > div:nth-child(7) > div.lesson-card > div")
         self.last_video_play = (By.CSS_SELECTOR, "#app > div > div.videolesson > div > div:nth-child(2) > div > div:nth-child(3) > div.video-player-proweb > div > div.video-player-proweb__controlls > div.video-player-proweb__controllers > div.video-player-proweb__controllers-left > button")
         self.last_video_fullscreen = (By.CSS_SELECTOR, "#app > div > div.videolesson > div > div:nth-child(2) > div > div:nth-child(3) > div.video-player-proweb > div > div.video-player-proweb__controlls > div.video-player-proweb__controllers > div.video-player-proweb__controllers-right > button.baseicon.baseavatar_fullscreen")
-        # self.last_video_stop = (By.CSS_SELECTOR, "#app > div > div.videolesson > div > div:nth-child(2) > div > div:nth-child(3) > div.video-player-proweb > div > video")
-        # self.last_video_fullscreen_exit = (By.CSS_SELECTOR, "#app > div > div.videolesson > div > div:nth-child(2) > div > div:nth-child(3) > div.video-player-proweb > div > div.video-player-proweb__controlls > div.video-player-proweb__controllers > div.video-player-proweb__controllers-right > button.baseicon.baseavatar_fullscreen-exit")
         self.last_pause = (By.CSS_SELECTOR, "#app > div > div.videolesson > div > div:nth-child(2) > div > div:nth-child(3) > div.video-player-proweb > div > video")
         self.profile_icon = (By.CSS_SELECTOR, "#app > div > div.header > div > div.header__avatar > div")
         self.exit_btn = (By.CSS_SELECTOR, "#app > div > div.inforation > div > div > div:nth-child(5) > div")
@@ -34,17 +32,10 @@ class LessonsPage:
         wait = WebDriverWait(self.driver, 10)
         wait.until(EC.element_to_be_clickable(self.last_video_fullscreen)).click()
 
-    # def click_last_video_stop(self):
-    #     wait = WebDriverWait(self.driver, 10)
-    #     wait.until(EC.element_to_be_clickable(self.last_video_stop)).click()
-
     def click_last_pause(self):
         wait = WebDriverWait(self.driver, 10)
         element = wait.until(EC.element_to_be_clickable(self.last_pause))
         ActionChains(self.driver).double_click(element).perform()
-    # def click_last_video_fullscreen_exit(self):
-    #     wait = WebDriverWait(self.driver, 10)
-    #     wait.until(EC.element_to_be_clickable(self.last_video_fullscreen_exit))
 
 
     def click_profile_icon(self):
