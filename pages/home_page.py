20b31a9c1bb2329a353339ee8bb0ca85f532047d
Sardorbek27 <bg0241767@gmail.com>
2026-03-04 14:33:47 +0500

    first commit

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,35 +1,31 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver import ActionChains
 
-class HomePage():
+class HomePage:
     def __init__(self, driver):
         self.driver = driver
         self.video_instruction = (By.CSS_SELECTOR, "#app > div > div.home-content > div > div > div > div > div.home__education > div.home__education-page > div > div.home-card.pointer")
-        self.fullscreen_btn = (By.CSS_SELECTOR, "#app > div > div.home-instruction > div > div.home-instruction__content-main > div > div > div.video-player-proweb__controlls > div.video-player-proweb__controllers > div.video-player-proweb__controllers-right > button:nth-child(5)")
-        self.pause = (By.CSS_SELECTOR, "#app > div > div.home-instruction > div > div.home-instruction__content-main > div > div > div.video-player-proweb__actinview")
-        self.fullscreen_exit = (By.CSS_SELECTOR, "#app > div > div.home-instruction > div > div.home-instruction__content-main > div > div > div.video-player-proweb__controlls > div.video-player-proweb__controllers > div.video-player-proweb__controllers-right > button:nth-child(5)")
-        self.main_btn = (By.CSS_SELECTOR, "#app > div > div.layout > div > div.material-dialog__window > div > ul > li:nth-child(1)")
-        self.group_card = (By.CSS_SELECTOR, "#app > div > div.home-content > div > div > div > div > div.home__education > div.home__education-page > div > div:nth-child(2) > img")
-        self.profile_icon = (By.CSS_SELECTOR, "#app > div > div.header > div > div.header__avatar")
-        self.exit_btn = (By.CSS_SELECTOR, "#app > div > div.inforation > div > div > div:nth-child(5) > div")
-        self.confirm_exit = (By.CSS_SELECTOR, "#dialog > div > div > div.material-dialog__window-actions > button:nth-child(2)")
+        self.pause = (By.CSS_SELECTOR, "#app > div > div.home-instruction > div > div.home-instruction__content-main > div > div > video")
+        self.main_btn = (By.CSS_SELECTOR, "#app > div > div.layout > div > div.material-dialog__window > div > ul > li:nth-child(1) > div")
+        self.group_card = (By.CSS_SELECTOR, "#app > div > div.home-content > div > div > div > div > div.home__education > div.home__education-page > div > div:nth-child(2)")
+
+
 
     def click_video_instruction(self):
         wait = WebDriverWait(self.driver, 10)
         wait.until(EC.element_to_be_clickable(self.video_instruction)).click()
 
-    def click_fullscreen_btn(self):
-        wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(self.fullscreen_btn)).click()
-
     def click_pause(self):
         wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(self.pause)).click()
+        element = wait.until(EC.element_to_be_clickable(self.pause))
+        ActionChains(self.driver).double_click(element).perform()
 
-    def click_fullscreen_exit(self):
+    def click_pause_exit(self):
         wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(self.fullscreen_exit)).click()
+        element = wait.until(EC.element_to_be_clickable(self.pause))
+        ActionChains(self.driver).double_click(element).perform()
 
     def click_main_btn(self):
         wait = WebDriverWait(self.driver, 10)
@@ -40,14 +36,3 @@ class HomePage():
         wait.until(EC.element_to_be_clickable(self.group_card)).click()
 
 
-    def click_profile_icon(self):
-        wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(self.profile_icon)).click()
-
-    def click_exit_btn(self):
-        wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(self.exit_btn)).click()
-
-    def click_confirm_exit(self):
-        wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(self.confirm_exit)).click()
--- pages/lessons_page.py
+++ pages/lessons_page.py
@@ -1,42 +1,61 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver import ActionChains
 
-class LessonsPage():
+class LessonsPage:
     def __init__(self, driver):
         self.driver = driver
-        self.lessons_btn = (By.CSS_SELECTOR, '#tabbar > div > div.tab-header > div.tab-header__wrapper > div:nth-child(2)')
-        self.last_card = (By.CSS_SELECTOR, '#app > div > div.container.container_mobile > div > div > div.new-lessons_content > div > div:nth-child(5) > div.flex.gap20 > div:nth-child(2) > div.lesson-card > div')
-        self.fullscreen_btn = (By.CSS_SELECTOR, '#app > div > div.videolesson > div > div:nth-child(2) > div > div:nth-child(3) > div.video-player-proweb > div > div.video-player-proweb__controlls > div.video-player-proweb__controllers > div.video-player-proweb__controllers-right > button:nth-child(3)')
-        self.play_btn = (By.CSS_SELECTOR, '#app > div > div.videolesson > div > div:nth-child(2) > div > div:nth-child(3) > div.video-player-proweb > div > div.video-player-proweb__controlls > div.video-player-proweb__controllers > div.video-player-proweb__controllers-left > button')
-        self.pause = (By.CSS_SELECTOR, '#app > div > div.videolesson > div > div:nth-child(2) > div > div:nth-child(3) > div.video-player-proweb > div > div.video-player-proweb__actinview')
-        self.fullscreen_exit = (By.CSS_SELECTOR, "#app > div > div.videolesson > div > div:nth-child(2) > div > div:nth-child(3) > div.video-player-proweb > div > div.video-player-proweb__controlls > div.video-player-proweb__controllers > div.video-player-proweb__controllers-right > button:nth-child(3)")
+        self.nav_lesson = (By.CSS_SELECTOR, "#tabbar > div > div.tab-header > div.tab-header__wrapper > div:nth-child(2)")
+        self.last_lesson = (By.CSS_SELECTOR, "#app > div > div.container.container_mobile > div > div > div.new-lessons_content > div > div:nth-child(5) > div.flex.gap20 > div:nth-child(7) > div.lesson-card > div")
+        self.last_video_play = (By.CSS_SELECTOR, "#app > div > div.videolesson > div > div:nth-child(2) > div > div:nth-child(3) > div.video-player-proweb > div > div.video-player-proweb__controlls > div.video-player-proweb__controllers > div.video-player-proweb__controllers-left > button")
+        self.last_video_fullscreen = (By.CSS_SELECTOR, "#app > div > div.videolesson > div > div:nth-child(2) > div > div:nth-child(3) > div.video-player-proweb > div > div.video-player-proweb__controlls > div.video-player-proweb__controllers > div.video-player-proweb__controllers-right > button.baseicon.baseavatar_fullscreen")
+        # self.last_video_stop = (By.CSS_SELECTOR, "#app > div > div.videolesson > div > div:nth-child(2) > div > div:nth-child(3) > div.video-player-proweb > div > video")
+        # self.last_video_fullscreen_exit = (By.CSS_SELECTOR, "#app > div > div.videolesson > div > div:nth-child(2) > div > div:nth-child(3) > div.video-player-proweb > div > div.video-player-proweb__controlls > div.video-player-proweb__controllers > div.video-player-proweb__controllers-right > button.baseicon.baseavatar_fullscreen-exit")
+        self.last_pause = (By.CSS_SELECTOR, "#app > div > div.videolesson > div > div:nth-child(2) > div > div:nth-child(3) > div.video-player-proweb > div > video")
+        self.profile_icon = (By.CSS_SELECTOR, "#app > div > div.header > div > div.header__avatar > div")
+        self.exit_btn = (By.CSS_SELECTOR, "#app > div > div.inforation > div > div > div:nth-child(5) > div")
+        self.confirm_exit = (By.CSS_SELECTOR, "#dialog > div > div > div.material-dialog__window-actions > button:nth-child(2)")
 
 
-    def click_lessons_btn(self):
+    def click_nav_lesson(self):
         wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(self.lessons_btn)).click()
+        wait.until(EC.element_to_be_clickable(self.nav_lesson)).click()
 
-    def click_last_card(self):
+    def click_last_lesson(self):
         wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(self.last_card)).click()
+        wait.until(EC.element_to_be_clickable(self.last_lesson)).click()
 
-    def click_fullscreen_btn(self):
+    def click_last_video_play(self):
         wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(self.fullscreen_btn)).click()
+        wait.until(EC.element_to_be_clickable(self.last_video_play)).click()
 
-    def click_play_btn(self):
+    def click_last_video_fullscreen(self):
         wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(self.play_btn)).click()
+        wait.until(EC.element_to_be_clickable(self.last_video_fullscreen)).click()
 
-    def click_pause(self):
+    # def click_last_video_stop(self):
+    #     wait = WebDriverWait(self.driver, 10)
+    #     wait.until(EC.element_to_be_clickable(self.last_video_stop)).click()
+
+    def click_last_pause(self):
         wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(self.pause)).click()
+        element = wait.until(EC.element_to_be_clickable(self.last_pause))
+        ActionChains(self.driver).double_click(element).perform()
+    # def click_last_video_fullscreen_exit(self):
+    #     wait = WebDriverWait(self.driver, 10)
+    #     wait.until(EC.element_to_be_clickable(self.last_video_fullscreen_exit))
 
-    def click_fullscreen_exit(self):
+
+    def click_profile_icon(self):
         wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(self.fullscreen_btn)).click()
+        wait.until(EC.element_to_be_clickable(self.profile_icon)).click()
 
+    def click_exit_btn(self):
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.element_to_be_clickable(self.exit_btn)).click()
 
-
+    def click_confirm_exit(self):
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.element_to_be_clickable(self.confirm_exit)).click()
 
